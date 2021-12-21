1a1811414f0f164f567200365df1f081a7647444
rizaluardi <rizaluardi@gmail.com>
2021-12-21 14:26:27 +0700

    Rizaluardi
    Rizaluardi

---
+++ app/src/main/java/com/appsmor/rizalmynavigation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.appsmor.rizalmynavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
